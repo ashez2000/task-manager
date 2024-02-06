66d234d7ba76b6c73a5dc1da1d8357f8f5fa5b34
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-02-06 17:27:00 +0530

    Add in-memory store for tasks

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,25 +1,74 @@
 import { Router } from 'express'
+import taskStore from '../store/task.js'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send('get all tasks')
+  const tasks = taskStore.findMany()
+  res.status(200).json(tasks)
 })
 
 router.post('/', (req, res) => {
-  res.status(201).send('create task')
+  const { title, description, completed } = req.body
+
+  const task = taskStore.create(title, description, completed)
+  res.status(201).send(task)
 })
 
 router.get('/:id', (req, res) => {
-  res.send('get task by id')
+  const id = parseInt(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: 'invalid id',
+    })
+  }
+
+  const task = taskStore.findById(id)
+  if (!task) {
+    return res.status(404).json({
+      message: 'task not found',
+    })
+  }
+
+  res.status(200).json(task)
 })
 
 router.put('/:id', (req, res) => {
-  res.send('update task by id')
+  const id = parseInt(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: 'invalid id',
+    })
+  }
+
+  const { title, description, completed } = req.body
+
+  const task = taskStore.updateById(id, title, description, completed)
+  if (!task) {
+    return res.status(404).json({
+      message: 'task not found',
+    })
+  }
+
+  res.status(200).json(task)
 })
 
 router.delete('/:id', (req, res) => {
-  res.send('delete task by id')
+  const id = parseInt(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: 'invalid id',
+    })
+  }
+
+  const task = taskStore.deleteById(id)
+  if (!task) {
+    return res.status(404).json({
+      message: 'task not found',
+    })
+  }
+
+  res.status(200).json(task)
 })
 
 export default router
---
+++ src/store/task.ts
@@ -0,0 +1,89 @@
+import fs from 'node:fs'
+import crypto from 'node:crypto'
+
+const tasks = JSON.parse(fs.readFileSync('./data/tasks.json', 'utf8')) as Task[]
+
+export type Task = {
+  id: number
+  title: string
+  description: string
+  completed: boolean
+}
+
+class TaskStore {
+  private store = new Map<number, Task>()
+
+  constructor(tasks?: Task[]) {
+    if (tasks) {
+      for (const t of tasks) {
+        this.store.set(t.id, t)
+      }
+    }
+  }
+
+  findMany(): Task[] {
+    return [...this.store.values()]
+  }
+
+  findById(id: number): Task | null {
+    const task = this.store.get(id)
+    if (!task) {
+      return null
+    }
+
+    return task
+  }
+
+  create(title: string, description: string, completed: boolean): Task {
+    const id = crypto.randomInt(10_000)
+
+    const task = {
+      id,
+      title,
+      description,
+      completed,
+    }
+
+    this.store.set(id, task)
+
+    return task
+  }
+
+  updateById(
+    id: number,
+    title: string,
+    description: string,
+    completed: boolean
+  ): Task | null {
+    const task = this.store.get(id)
+    if (!task) {
+      return null
+    }
+
+    const update = {
+      id,
+      title,
+      description,
+      completed,
+    }
+
+    this.store.set(id, update)
+
+    return task
+  }
+
+  deleteById(id: number) {
+    const task = this.store.get(id)
+    if (!task) {
+      return null
+    }
+
+    this.store.delete(id)
+
+    return task
+  }
+}
+
+const taskStore = new TaskStore(tasks)
+
+export default taskStore
